e3298538f8ffb623becd74685caaa0c6ca3fd913
Matt <matthewha1993@gmail.com>
2018-06-11 00:50:16 -0700

    Create StaticPagesController and  ro root and app front end pages

---
+++ frontend/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import {Route, Switch} from 'react-router-dom';
+// import {AuthRoute, ProtectedRoute} from '../util/route_util';
+
+// Components
+
+
+const App = ({children}) => (
+  <div>Hi mom!</div>
+);
+
+
+export default App;
