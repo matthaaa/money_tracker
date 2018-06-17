4df7c870e240f3bf4bb62763b2b8c459afbf2cea
Matt <matthewha1993@gmail.com>
2018-06-16 22:07:20 -0700

    Recreate user controller with proper methods

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,22 @@
 class Api::UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def show
+  end
 end
