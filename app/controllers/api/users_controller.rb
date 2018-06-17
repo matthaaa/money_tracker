1935921ff27ca5a2eeea5a78ee7869946c56d061
Matt <matthewha1993@gmail.com>
2018-06-17 01:04:43 -0700

    Create UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,17 @@
 class Api::UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 400
+    end
   end
 
   def edit
@@ -12,11 +21,25 @@ class Api::UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user
+      render :show
+    else
+      render json: 'User not found!', status: 404
+    end
   end
 
   def destroy
   end
 
-  def show
+  private
+  def user_params
+    params.require(:user).permit(
+      :username, :password, :full_name, :email, :img_url,
+    )
   end
 end
