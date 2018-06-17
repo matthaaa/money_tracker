17d0d3dbaea95337f5f6f14d6bb1f20a733f885e
Matt <matthewha1993@gmail.com>
2018-06-16 22:08:17 -0700

    Create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
+    get 'sessions/new'
+  end
+
+  namespace :api do
+    get 'sessions/create'
+  end
+
+  namespace :api do
+    get 'sessions/destroy'
+  end
+
+  namespace :api do
     get 'users/new'
   end
 
