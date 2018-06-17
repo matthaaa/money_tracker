198a760c52cf5fb2aa2e7325196229569746faf3
Matt <matthewha1993@gmail.com>
2018-06-17 01:03:29 -0700

    Create SessionsController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,8 +3,33 @@ class Api::SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_creds(
+      params[:user][:username],
+      params[:user][:password],
+    )
+
+    if @user
+      login(@user)
+      render 'api/users/show'
+    elsif !User.find_by_username(params[:user][:username])
+      render json: [
+        'The username you entered doesn\'t belong to an account. ' +
+        'Please check your username and try again.'
+      ], status: 404
+    else
+      render json: [
+        'Sorry, your password was incorrect. Please double-check ' +
+        'your password.'
+      ], status: 404
+    end
   end
 
   def destroy
+    if logged_in?
+      logout
+      render json: {}
+    else
+      render json: ['No user to sign out'], status: 400
+    end
   end
 end
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,15 +3,14 @@ import {Route, Switch} from 'react-router-dom';
 // import {AuthRoute, ProtectedRoute} from '../util/route_util';
 
 // Components
-// import SignupViewContainer from './user_views/signup_view_container';
+import SignUpView from './user_views/signup_view';
 
 
 const App = ({children}) => (
   <div>
     <section className="main-view">
       <Switch>
-        // <Route exact path="/signup" component={SignupViewContainer} />
-        // <Route exact path="/login" component={LoginViewContainer} />
+        <Route exact path="/signup" component={SignUpView} />
       </Switch>
     </section>
   </div>
