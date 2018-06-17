f932927309f7fa3c43764a7cf8260d899a16dfde
Matt <matthewha1993@gmail.com>
2018-06-16 22:08:54 -0700

    Clean up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'sessions/new'
-  end
-
-  namespace :api do
-    get 'sessions/create'
-  end
-
-  namespace :api do
-    get 'sessions/destroy'
-  end
-
-  namespace :api do
-    get 'users/new'
-  end
-
-  namespace :api do
-    get 'users/create'
-  end
-
-  namespace :api do
-    get 'users/edit'
-  end
-
-  namespace :api do
-    get 'users/update'
-  end
-
-  namespace :api do
-    get 'users/index'
-  end
-
-  namespace :api do
-    get 'users/destroy'
-  end
-
-  namespace :api do
-    get 'users/show'
-  end
-
   namespace :api, defaults: {format: :json} do
+    resource :session, only: [:create, :destroy]
+    resources :users
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
