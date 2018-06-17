65cb8af96894e2dcb87f2667d5f5f248bb9527bd
Matt <matthewha1993@gmail.com>
2018-06-17 00:11:46 -0700

    Add Switch component and tentative route and import

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-// import {Route, Switch} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 // import {AuthRoute, ProtectedRoute} from '../util/route_util';
 
 // Components
+// import SignupViewContainer from './user_views/signup_view_container';
 
 
 const App = ({children}) => (
-  <div>Hi mom!</div>
+  <div>
+    <section className="main-view">
+      <Switch>
+        // <Route exact path="/signup" component={SignupViewContainer} />
+        // <Route exact path="/login" component={LoginViewContainer} />
+      </Switch>
+    </section>
+  </div>
 );
 
 
