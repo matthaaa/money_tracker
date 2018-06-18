94c10a5f095cde8198fecc3fa2d0cec1e5412e1b
Matt <matthewha1993@gmail.com>
2018-06-17 19:43:36 -0700

    Create main dashboard container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-// import {AuthRoute, ProtectedRoute} from '../util/route_util';
+import {AuthRoute, ProtectedRoute} from '../util/route_util';
 
 // Components
 import SignUpViewContainer from './user_views/signup_view_container';
@@ -11,8 +11,9 @@ const App = ({children}) => (
   <div>
     <section className="main-view">
       <Switch>
-        <Route exact path="/signup" component={SignUpViewContainer} />
-        <Route exact path="/login" component={LoginViewContainer} />
+        <ProtectedRoute exact path="/" component={} />
+        <AuthRoute exact path="/signup" component={SignUpViewContainer} />
+        <AuthRoute exact path="/login" component={LoginViewContainer} />
       </Switch>
     </section>
   </div>
