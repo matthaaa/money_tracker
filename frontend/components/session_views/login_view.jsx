9545cc7cdff85769f8db56e9e761d573b69d35cf
Matt <matthewha1993@gmail.com>
2018-06-28 16:28:47 -0700

    Add animation to login screen

--- frontend/components/session_views/login_view.jsx
+++ frontend/components/session_views/login_view.jsx
@@ -47,8 +47,8 @@ class LoginView extends Component {
   // ==================================================
   render() {
     return (
-      <main className="logged-out-view">
-        <div className="login-view-main-content fadeIn">
+      <main className="logged-out-view fadeIn">
+        <div className="login-view-main-content">
           <LoginForm
             errors={this.props.errors}
             formUser={this.state}
