d1eaa152535543d47995d7a1329bdfe957a67b82
Matt <matthewha1993@gmail.com>
2018-07-03 19:54:23 -0700

    Run migration for adding budget_id to cells column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180704021217) do
+ActiveRecord::Schema.define(version: 20180704025214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180704021217) do
     t.string "categories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "budget_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
