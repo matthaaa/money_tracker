d4e4122a78b8ecdf4950961a59df11a89fe2f047
Matt <matthewha1993@gmail.com>
2018-07-03 19:51:07 -0700

    Set up associations for Cell and Budget models

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,2 +1,18 @@
 class Cell < ApplicationRecord
+  validates :user_id, presence: true
+  # ==================================================
+  # Associations
+  # ==================================================
+  belongs_to :budget,
+    dependent: :destroy,
+    primary_key: :id,
+    foreign_key: :budget_id,
+    class_name: :Budget,
+
+  has_many :cells,
+    dependent: :destroy,
+    primary_key: :id,
+    foreign_key: :budget_id,
+    class_name: :Cell
+
 end
