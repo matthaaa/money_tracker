b183186bac5f2dd360a08af5b0c30a6d1e43f34a
Matt <matthewha1993@gmail.com>
2018-07-03 19:05:55 -0700

    Create migration for Rows

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resource :session, only: [:create, :destroy]
     resources :users
     resources :budgets
+    resources :rows
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
