c37e77b02d11315b44be9b811ac3c7a164328f34
Matt <matthewha1993@gmail.com>
2018-07-03 18:27:16 -0700

    Add association for users having budgets

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,3 +1,13 @@
 class Budget < ApplicationRecord
   validates :user_id, presence: true
+
+  # ==================================================
+  # Associations
+  # ==================================================
+  belongs_to :user,
+    dependent: :destroy,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+    
 end
