2d8043d46d57d3a66b152bfcb83b7bf249eb41d1
Matt <matthewha1993@gmail.com>
2018-07-03 20:50:41 -0700

    Fix columns namespace in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,10 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'columns/new'
-  end
-
-  namespace :api do
-    get 'columns/create'
-  end
-
-  namespace :api do
-    get 'columns/edit'
-  end
-
-  namespace :api do
-    get 'columns/update'
-  end
-
-  namespace :api do
-    get 'columns/destroy'
-  end
-
-  namespace :api do
-    get 'columns/show'
-  end
-
-  namespace :api do
-    get 'columns/index'
-  end
-
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
     resources :users
     resources :budgets
     resources :cells
+    resources :columns
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
