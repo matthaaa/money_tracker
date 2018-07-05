3dd455a2e3daf980523950ce549ee7fccb48d129
Matt <matthewha1993@gmail.com>
2018-07-05 16:58:27 -0700

    Import link to home view

--- frontend/components/dashboard_view.jsx
+++ frontend/components/dashboard_view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 // Components
 import MainHeaderContainer from './generic/main_header_container';
