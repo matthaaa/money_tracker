1b4f3698ce20be46f6fc5b8f58d92ae1a393072d
Matt <matthewha1993@gmail.com>
2018-07-04 23:10:56 -0700

    Add column_id to cells and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180705060712) do
+ActiveRecord::Schema.define(version: 20180705060946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180705060712) do
     t.string "categories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "column_id", null: false
   end
 
   create_table "columns", force: :cascade do |t|
