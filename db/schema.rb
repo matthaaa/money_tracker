d46086e09b5f37543b1228d52e21dc05611eddee
Matt <matthewha1993@gmail.com>
2018-07-04 23:09:12 -0700

    Run migration and update schema for cells

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180704035740) do
+ActiveRecord::Schema.define(version: 20180705060712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20180704035740) do
     t.string "categories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "budget_id", null: false
   end
 
   create_table "columns", force: :cascade do |t|
