e3502b91da7a7385057b74c499331fd8d61f23a0
Matt <matthewha1993@gmail.com>
2018-07-05 17:19:12 -0700

    Create new cell input

---
+++ frontend/components/budget_views/forms/new_cell_form.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Component} from 'react';
+
+// Components
+import CustomButton from '../../generic/buttons/custom_button'
+import FormInput from '../../generic/forms/form_input'
+import FormErrors from '../../generic/forms/form_errors'
+
+
+class NewCellForm extends Component {
+
+  // ==================================================
+  // Render
+  // ==================================================
+  renderValueInput(formCell, onUpdate) {
+    return (
+      <FormInput
+        placeholder={""}
+        contentClassName={"new-cell-input"}
+        value={formUser.password}
+        onChange={onUpdate('value')}
+      />
+    );
+  }
+
+  render() {
+    const {
+      errors,
+      formUser,
+      onUpdate,
+      onSubmit,
+    } = this.props;
+
+    return (
+      <form className="cell-form-content">
+        {this.renderValueInput(formCell, onUpdate)}
+        <CustomButton
+          className="new-cell-submit-button"
+          text={"Add Cell"}
+          onPress={onSubmit}
+        />
+        <FormErrors errors={errors} />
+      </form>
+    );
+  }
+
+};
+
+
+export default NewCellForm;
