2449bf4980e9a71cdc03a41e43326fd02c0e8f47
Matt <matthewha1993@gmail.com>
2018-07-07 16:24:28 -0700

    Add thunk for getting all budgets to actions file for budgets

--- frontend/actions/budget_actions.js
+++ frontend/actions/budget_actions.js
@@ -2,10 +2,16 @@ import * as BudgetAPIUtil from '../util/budget_api_util';
 
 
 export const RECEIVE_BUDGET = 'RECEIVE_BUDGET';
+export const RECEIVE_BUDGETS = 'RECEIVE_BUDGETS';
 
 export const addBudget = (budget) => dispatch => (
   BudgetAPIUtil.createBudget(budget).then(payload => dispatch(receiveBudget(payload)))
 );
+
+export const receiveBudgets = (budgets) => ({
+  type: RECEIVE_BUDGETS,
+  budgets,
+})
 
 export const receiveBudget = (payload) => ({
   type: RECEIVE_BUDGET,
