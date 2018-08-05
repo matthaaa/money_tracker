7f24e5df40da58272546839dcd37bd17b20d67a1
Matt <matthewha1993@gmail.com>
2018-08-05 15:00:59 -0800

    Add todos for commented out associations

--- app/models/column.rb
+++ app/models/column.rb
@@ -8,6 +8,7 @@ class Column < ApplicationRecord
     foreign_key: :budget_id,
     class_name: :Budget,
 
+  # TODO: Remove these if columns are not used and removed from schema.
   # has_many :cells,
   #   dependent: :destroy,
   #   primary_key: :id,
