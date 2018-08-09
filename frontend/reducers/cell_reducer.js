deae5a96c909f40b0aca8567b86503960d1c78c1
Matt <matthewha1993@gmail.com>
2018-08-09 15:30:34 -0800

    Create cell reducer switch case ffor receive cell and cells

--- frontend/reducers/cell_reducer.js
+++ frontend/reducers/cell_reducer.js
@@ -4,3 +4,16 @@ import {
   RECEIVE_CELLS,
   RECEIVE_CELL,
 } from '../actions/cell_actions';
+
+const cellReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+
+  switch(action.type) {
+      case RECEIVE_CELLS:
+        return merge({}, action.cells);
+      case RECEIVE_CELL:
+        return merge({}, oldState, {[action.payload.cell.id]: action.payload.cell});
+      default:
+        return oldState;
+  }
+}
