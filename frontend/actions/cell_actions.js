eb05e11021486098f1d35b4da17f7859c062ae41
Matt <matthewha1993@gmail.com>
2018-08-09 15:24:05 -0800

    Create actions for fetchCells

--- frontend/actions/cell_actions.js
+++ frontend/actions/cell_actions.js
@@ -1,1 +1,14 @@
 import * CellAPIUtil from '../util/cell_api_util';
+
+
+export const RECEIVE_CELLS = 'RECEIVE_CELLS';
+export const RECEIVE_CELL = 'RECEIVE_CELL';
+
+export const requestCells = () => dispatch => (
+  CellAPIUtil.fetchCells().then(cells => dispatch(receiveCells(cells)))
+)
+
+export const receiveCells = (cells) => ({
+  type: RECEIVE_CELLS,
+  cells,
+})
