8349679fa89fdbb79ff9a9e0753963ba794bff16
Matt <matthewha1993@gmail.com>
2018-08-09 22:48:04 -0800

    Create budget name form

---
+++ frontend/components/budget_views/forms/new_budget_form.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Component} from 'react';
+
+// Components
+import CustomButton from '../../generic/buttons/custom_button'
+import FormInput from '../../generic/forms/form_input'
+import FormErrors from '../../generic/forms/form_errors'
+
+
+class NewBudgetForm extends Component {
+
+  // ==================================================
+  // Render
+  // ==================================================
+  renderNameInput(formBudget, onUpdate) {
+    return (
+      <FormInput
+        placeholder={""}
+        contentClassName={"new-budget-input"}
+        value={formBudget.name}
+        onChange={onUpdate('name')}
+      />
+    );
+  }
+
+  render() {
+    const {
+      errors,
+      formBudget,
+      onUpdate,
+      onSubmit,
+    } = this.props;
+
+    return (
+      <form className="budget-form-content">
+        {this.renderNameInput(formBudget, onUpdate)}
+        <CustomButton
+          className="new-budget-submit-button"
+          text={"Continue"}
+          onPress={onSubmit}
+        />
+        <FormErrors errors={errors} />
+      </form>
+    );
+  }
+
+};
+
+
+export default NewBudgetForm;
